73e0892502dc9374eed0150773b0f2c2146461bf
Kwang_Le <quangle280302@gmail.com>
2023-10-16 14:46:47 +0700

    add product-detail component

---
+++ src/components/product-detail/BottomNavBar.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="bottom-nav fixed-bottom nav d-flex justify-content-center align-items-center">
+    <div class="service-btn col-2 d-flex justify-content-center flex-column align-items-center ">
+        <i class="bi bi-chat-dots"></i>
+        <p>서비스센터</p>
+    </div>
+    <div class="order-track-btn col-2 d-flex justify-content-center flex-column align-items-center ">
+        <i class="bi bi-file-text"></i>
+        <p>주문 조회</p>
+    </div>
+    <div class="buy-btn col-8 d-flex align-items-center justify-content-center">
+        즉시구매
+    </div>
+</div>
+</template>
+
+<style scoped>
+.bottom-nav {
+    padding: 3px 0;
+    margin: auto;
+    max-width: 640px;
+    background: white;
+    box-shadow: 0 -5px 5px -5px rgba(0,0,0,.3);
+    cursor: pointer;
+}
+.buy-btn {
+    background: -webkit-linear-gradient(left,#FE7144,#FD4969);
+    border-radius: 45px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 600;
+    height: 55px;
+}
+</style>
---
+++ src/components/product-detail/ReviewCommentsSection.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Comment from './Comment.vue';
+</script>
+
+<template>
+    <div class="card" style="width: 640px;">
+        <Comment></Comment>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
